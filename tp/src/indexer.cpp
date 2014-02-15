176757fb1581120573ca505ec08bad6ad8ae76c6
Carlos Filho <carlosfilho88@gmail.com>
2014-02-15 14:04:28 -0200

    New example

--- tp/src/indexer.cpp
+++ tp/src/indexer.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <CollectionReader.h>
 #include <CollectionWriter.h>
-#include <htmlcxx/html/ParserDom.h>
+#include <ParserDom.h>
 
 using namespace std;
 using namespace htmlcxx;
@@ -14,7 +14,35 @@ int main(int argc, char *argv[]) {
   cr.getNextDocument(d);
 
   string html = d.getText();
-  HTML::ParserDom parser;
+    HTML::ParserDom parser;
   tree<HTML::Node> dom = parser.parseTree(html);
-  cout << dom << endl;
+  
+  tree<HTML::Node>::iterator it = dom.begin();
+  tree<HTML::Node>::iterator end = dom.end();
+  for (; it != end; ++it) {
+    cout << "Tag=" << it->tagName() << " content=\"" << it->content(html) << "\"\n";
+      if (it->tagName() == "link") {
+        it->parseAttributes();
+
+        pair<bool, string> p = it->attribute("href");
+        if(p.first) {
+          cout << "Found a link: ";
+          cout << p.second.c_str() << endl;
+        }
+      } else if (it->tagName() == "body") {
+        cout << "Found body, text=\"" << it->text() << "\"" << " closing=\"" << it->closingText() << "\"\n";
+    } else {
+      cout << "Tag=" << it->tagName() << " content=\"" << it->content(html) << "\"\n";
+    }
+  }
+
+  it = dom.begin();
+  end = dom.end();
+  for (; it != end; ++it) {
+    if ((!it->isTag()) && (!it->isComment())) {
+      cout << "Found text: ";
+      cout << it->text() << endl;
+    }
+  }
+  return 0;
 }
