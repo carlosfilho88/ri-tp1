ae1490aa9b336baea592fa91831aae558bf09152
Carlos Filho <carlosfilho88@gmail.com>
2014-02-14 22:58:35 -0200

    Add htmlcxx parser

--- tp/src/indexer.cpp
+++ tp/src/indexer.cpp
@@ -2,11 +2,19 @@
 #include <stdio.h>
 #include <CollectionReader.h>
 #include <CollectionWriter.h>
+#include <htmlcxx/html/ParserDom.h>
+
+using namespace std;
+using namespace htmlcxx;
 
 int main(int argc, char *argv[]) {
   RICPNS::Document d;
   RICPNS::CollectionReader cr(argv[1], argv[2]);
 
   cr.getNextDocument(d);
-  std::cout << ">>> " << "[" << d.getText() << "];" << std::endl;
-}        
+
+  string html = d.getText();
+  HTML::ParserDom parser;
+  tree<HTML::Node> dom = parser.parseTree(html);
+  cout << dom << endl;
+}
